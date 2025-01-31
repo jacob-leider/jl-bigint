14d7a00db13d7c1dfcfb092b9ac16468241d0460
jacob-leider <102913421+jacob-leider@users.noreply.github.com>
2025-01-31 14:24:33 -0500

    Create add_sub_mul.h

---
+++ src/add_sub_mul.h
@@ -0,0 +1,14 @@
+#ifndef __JL_ADD_SUB_MUL_H__
+#define __JL_ADD_SUB_MUL_H__
+
+#include <stdint.h>
+#include <stdio.h>
+
+uint8_t add_bstrings(const uint8_t *x, const uint8_t *y, uint8_t *z,
+                     uint8_t *carry, size_t x_size, size_t y_size,
+                     size_t z_size);
+
+uint8_t sub_bstrings(const uint8_t *x, const uint8_t *y, uint8_t *z,
+                     uint8_t *carry, size_t x_size, size_t y_size,
+                     size_t z_size);
+#endif
