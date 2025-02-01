5f505d7c0d93ab2b3bc8db55468fd5253337d77f
jacob-leider <102913421+jacob-leider@users.noreply.github.com>
2025-02-01 16:18:12 -0500

    Update add_sub_mul.h
    Un-inline add and sub blocks. These will ultimately by inlined by hand anyway.

--- src/add_sub_mul.h
+++ src/add_sub_mul.h
@@ -11,4 +11,10 @@ uint8_t add_bstrings(const uint8_t *x, const uint8_t *y, uint8_t *z,
 uint8_t sub_bstrings(const uint8_t *x, const uint8_t *y, uint8_t *z,
                      uint8_t *carry, size_t x_size, size_t y_size,
                      size_t z_size);
+
+uint8_t sub_bstrings_nocheck(const uint8_t *x, const uint8_t *y, uint8_t *z,
+                             size_t x_size, size_t y_size, size_t z_size);
+
+uint8_t add_bstrings_nocheck(const uint8_t *x, const uint8_t *y, uint8_t *z,
+                             size_t x_size, size_t y_size, size_t z_size);
 #endif
