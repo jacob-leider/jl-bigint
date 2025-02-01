1ac9098a79971227f8420d4b6e82c3cee132f7c0
jacob-leider <102913421+jacob-leider@users.noreply.github.com>
2025-02-01 16:22:45 -0500

    Update testutils.h

--- test/testutils.h
+++ test/testutils.h
@@ -4,11 +4,7 @@
 #ifndef __JL_TESTUTILS_H__
 #define __JL_TESTUTILS_H__
 
-void report_endianness();
-
 int check_endianness();
-
-void swap_bytes(uint16_t *x);
 
 void printhex_le(const uint8_t *x, int num_bits);
 
